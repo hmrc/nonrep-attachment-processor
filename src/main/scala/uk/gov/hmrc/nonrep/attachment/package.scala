d5091fb408b167bbdc73694d2228c766537255d3
greg.gajda@digital.hmrc.gov.uk <29655695+greg-gajda@users.noreply.github.com>
2022-02-11 15:32:34 +0000

    NONPR-2427 Post PR review changes

--- src/main/scala/uk/gov/hmrc/nonrep/attachment/package.scala
+++ src/main/scala/uk/gov/hmrc/nonrep/attachment/package.scala
@@ -11,10 +11,6 @@ package object attachment {
   type EitherErr[T] = Either[ErrorMessage, T]
 
   case class ErrorMessage(message: String, code: StatusCode = StatusCodes.BadRequest, error: Option[Throwable] = None)
-
-  object ErrorMessage {
-    def apply(message: String, code: Int): ErrorMessage = ErrorMessage(message, StatusCode.int2StatusCode(code))
-  }
-
+  
   case class AttachmentInfo(key: String, content: Option[ByteString] = None)
 }
