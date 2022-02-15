c758148c3cbc82b37241cefed3c2d0f034fc067b
Leeroy Chiweshe <67266542+Leeroyc720@users.noreply.github.com>
2022-02-15 11:28:38 +0000

    NONPR-2525- Added settings into serviceConfig

--- src/main/scala/uk/gov/hmrc/nonrep/attachment/server/ServiceConfig.scala
+++ src/main/scala/uk/gov/hmrc/nonrep/attachment/server/ServiceConfig.scala
@@ -1,8 +1,10 @@
 package uk.gov.hmrc.nonrep.attachment
 package server
 
+import akka.stream.alpakka.sqs.{SqsPublishBatchSettings, SqsSourceSettings}
 import com.typesafe.config.{Config, ConfigFactory}
-import scala.jdk.CollectionConverters._
+
+import scala.concurrent.duration.DurationInt
 
 class ServiceConfig(val servicePort: Int = 8000) {
 
@@ -11,6 +13,17 @@ class ServiceConfig(val servicePort: Int = 8000) {
   val env: String = sys.env.get("ENV").getOrElse("local")
 
   val attachmentsBucket = s"$env-nonrep-attachment-data"
+
+  val settings = SqsSourceSettings()
+    .withWaitTime(20.seconds)
+    .withMaxBufferSize(100)
+    .withMaxBatchSize(10)
+    .withCloseOnEmptyReceive(false)
+    .withVisibilityTimeout(10.seconds)
+
+  val batchSettings =
+    SqsPublishBatchSettings()
+      .withConcurrentRequests(1)
 
   private val configFile = new java.io.File(s"/etc/config/CONFIG_FILE")
 
--- src/main/scala/uk/gov/hmrc/nonrep/attachment/service/Queue.scala
+++ src/main/scala/uk/gov/hmrc/nonrep/attachment/service/Queue.scala
@@ -2,15 +2,40 @@ package uk.gov.hmrc.nonrep.attachment
 package service
 
 import akka.NotUsed
-import akka.stream.scaladsl.{Flow, Source}
+import akka.stream.{ActorAttributes, Supervision}
+import akka.stream.alpakka.sqs.scaladsl.SqsSource
+import akka.stream.alpakka.sqs.{MessageAction, SqsSourceSettings}
+import akka.stream.scaladsl.{Flow, RestartSource, Sink, Source}
+import software.amazon.awssdk.services.s3.model.Event
 import software.amazon.awssdk.services.sqs.model.Message
+
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
 
 /**
  * It's an interim object before the final interface is delivered
  */
 
 trait Queue {
-  def getMessages: Source[Message, NotUsed]
+
+  def getMessages: Source[Message, NotUsed] = {
+    val supervisionStrategy: Supervision.Decider = Supervision.stoppingDecider
+    val sourceSettings = SqsSourceSettings()
+      .withWaitTime(10 seconds)
+      .withMaxBufferSize(150)
+      .withMaxBatchSize(10)
+    val sqsSource = SqsSource(
+      sqsTopicArn,
+      sourceSettings
+    ).withAttributes(ActorAttributes.supervisionStrategy(supervisionStrategy))
+
+    RestartSource.onFailuresWithBackoff(
+      minBackoff = 300.millis,
+      maxBackoff = 5.seconds,
+      randomFactor = 0.25
+    )(() => sqsSource)
+  }
 
   def parseMessages: Flow[Message, AttachmentInfo, NotUsed]
 
@@ -19,12 +44,33 @@ trait Queue {
 
 class QueueService extends Queue {
 
+//  private def getFlow: Flow[Message, AttachmentInfo, NotUsed] = {
+//
+//    def processMessage(message: Message): Future[Event] = {
+//      val event = Json.parse(message.body()).as[Event]
+//      println(s"Processing event => $event")
+//      Future.successful(event)
+//    }
+//
+//    Flow[Message].mapAsyncUnordered(8) {
+//      message =>
+//        for {
+//          _ <- processMessage(message)
+//          _ <- Future.successful(MessageAction.delete(message))
+//        } yield ()
+//    }
+//  }
+//
+//  getMessages
+//    .via(getFlow)
+//    .runWith(Sink.ignore)
+
   // make sure that SQS async client is created with important parameters taken from service config
 
   /*
    * all these implementations are likely to be replaced
    */
-  override def getMessages: Source[Message, NotUsed] = Source.empty
+//  override def getMessages: Source[Message, NotUsed] = Source.empty
 
   override def parseMessages: Flow[Message, AttachmentInfo, NotUsed] = Flow.fromFunction((m: Message) => AttachmentInfo(m.messageId(), ""))
 
