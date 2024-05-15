1ff44fcfdfb34af356b68d2bdc6aa50ed0d0a348
DanielConnelly <DanielConnelly@users.noreply.github.com>
2024-05-15 17:19:35 +0100

    [NONPR-3466][DC] remove commented code

--- src/main/scala/uk/gov/hmrc/nonrep/attachment/utils/ZipUtils.scala
+++ src/main/scala/uk/gov/hmrc/nonrep/attachment/utils/ZipUtils.scala
@@ -8,25 +8,7 @@ import scala.util.{Try, Using}
 
 object ZipUtils {
 
-//  val streamToString: Array[Byte] => String = stream => new String(stream)
-//  val streamToArrayOfBytes: Array[Byte] => Array[Byte] = stream => stream
-
-//  def readFromZipAsString(zipFile: Array[Byte], fileName: String): EitherErr[String] =
-//    Try {
-//      readFileFromZip(zipFile, fileName, streamToString)
-//    }.toEither.flatMap {
-//      case LazyList() => Left(ErrorMessage(s"Zip entry with name $fileName is empty"))
-//      case LazyList(x) => Right(x)
-//    }.left.map {
-//      case thr: Exception        => ErrorMessage(s"Failed while trying to read zip entry $fileName with error: ${thr.getMessage}", Some(thr))
-//      case Left(x: ErrorMessage) => x
-//      case _                     => ErrorMessage(s"Failed while trying to read zip entry $fileName")
-//    }
-
-  val DEFAULT_ZIP_ENTRY_SIZE = 10000000
-  private def defaultedZipEntrySize(e: ZipEntry) = if (e.getSize > 0) e.getSize.toInt else DEFAULT_ZIP_ENTRY_SIZE
-
-  def readFileFromZip[T](input: Array[Byte], name: String): Either[Throwable, Option[Array[Byte]]] =
+  def readFileFromZip(input: Array[Byte], name: String): Either[Throwable, Option[Array[Byte]]] =
      Using.Manager { use =>
       val zip = use(new ZipInputStream(new ByteArrayInputStream(input)))
       val content = LazyList
@@ -42,26 +24,4 @@ object ZipUtils {
 
       content
     }.toEither
-//       .left.map(e => ErrorMessage(s"error reading $name from bundle due to ${e}", Some(e), ERROR))
-//       .flatMap(_.toRight(ErrorMessage(s"can not find file $name in zip file", None, ERROR)))
-
-
-  /**
-   * Method which creates zip archive from provided entries
-   */
-//  def createZip(entries: BundleEntry*): Array[Byte] = {
-//    val stream = new ByteArrayOutputStream
-//    val zip = new ZipOutputStream(stream)
-//
-//    entries.foreach(entry => {
-//      val zipEntry = new ZipEntry(entry.name)
-//      zip.putNextEntry(zipEntry)
-//      zip.write(entry.content)
-//      zip.closeEntry()
-//    })
-//
-//    zip.close()
-//    stream.toByteArray
-//  }
-
 }
